9b60f033f3fd603ddce16bfd7074324c68832e11
iKhSh <khalidshiba95@gmail.com>
2024-02-20 13:28:18 +0100

    Initial Commit

---
+++ AI Image Generator/AI_Image_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Image_GeneratorApp.swift
+//  AI Image Generator
+//
+//  Created by Khalid Shiba on 20.02.24.
+//
+
+import SwiftUI
+
+@main
+struct AI_Image_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
