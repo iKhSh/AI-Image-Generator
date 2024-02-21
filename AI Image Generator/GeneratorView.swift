f8adf9c9a76c349c8613058c112f5874c2c80d6d
iKhSh <khalidshiba95@gmail.com>
2024-02-21 14:54:34 +0100

    App Layout

---
+++ AI Image Generator/GeneratorView.swift
@@ -0,0 +1,18 @@
+//
+//  GeneratorView.swift
+//  AI Image Generator
+//
+//  Created by Khalid Shiba on 21.02.24.
+//
+
+import SwiftUI
+
+struct GeneratorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneratorView()
+}
